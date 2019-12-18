4dee3e8794697b9bc456bbde6b540025d0a78ea9
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-18 06:20:43 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -28,22 +28,3 @@ def my_all?(collection)
   end
 end
 
-def my_all?(collection)
-  i = 0
-  while i < collection.length
-    yield(collection[i])
-    i = i + 1
-  end
-end
-my_all?([1,2,3]) {|i| i < 2}
-
-def my_all?([1,2,3])
-  i = 0
-  while i < 3
-    yield(1)
-    i = 0 + 1
-  end
-end
-my_all?([1,2,3]) {|1| 1 < 2}
-  #=> true
-
